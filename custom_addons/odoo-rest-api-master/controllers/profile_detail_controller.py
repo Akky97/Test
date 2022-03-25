0db5d4a154aaa6a36f3a904ce526c9e1bbeda000
Aman Sharma <aman@aman.sharma>
2022-03-25 15:56:02 +0530

    Added changes in delete address api

--- custom_addons/odoo-rest-api-master/controllers/profile_detail_controller.py
+++ custom_addons/odoo-rest-api-master/controllers/profile_detail_controller.py
@@ -102,7 +102,7 @@ class OdooAPI(http.Controller):
             res_id.sudo().write({"public": True})
             if records:
                 for rec in records:
-                    sale = request.env['sale.order'].sudo().search([('partner_id','=',rec.id),('partner_invoice_id','=',rec.id),('partner_shipping_id','=',rec.id)])
+                    sale = request.env['sale.order'].sudo().search(['|', ('partner_id', '=', rec.id), '|', ('partner_invoice_id', '=', rec.id), ('partner_shipping_id', '=', rec.id)])
                     if sale:
                         msg = {"message": "Can not Update Your address Because it's in use.", "status_code": 400}
                         return return_Response_error(msg)
@@ -176,8 +176,7 @@ class OdooAPI(http.Controller):
             return error_response(e, msg)
         try:
             if records:
-                # order = request.env['sale.order'].sudo().search([('partner_id', '=', id)])
-                order = request.env['sale.order'].sudo().search([('partner_id', '=', id), ('partner_invoice_id', '=', id), ('partner_shipping_id', '=', id)])
+                order = request.env['sale.order'].sudo().search(['|', ('partner_id', '=', id), '|', ('partner_invoice_id', '=', id), ('partner_shipping_id', '=', id)])
                 if order:
                     error = {"message": "You cannot delete this address", "status": 400}
                     return return_Response_error(error)
