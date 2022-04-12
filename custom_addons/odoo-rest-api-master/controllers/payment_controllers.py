ea86073f69b560200c8e3bec4902a5a7e8b0cc0a
Aman Sharma <aman@aman.sharma>
2022-04-12 10:20:33 +0530

    Changes for success page

--- custom_addons/odoo-rest-api-master/controllers/payment_controllers.py
+++ custom_addons/odoo-rest-api-master/controllers/payment_controllers.py
@@ -33,7 +33,8 @@ def create_checkout_session(jdata):
                 ],
                 customer_email=request.env.user.login,
                 mode='payment',
-                success_url='https://pandostores.com/shop/success',
+                # success_url='https://pandostores.com/shop/success',
+                success_url='http://localhost:4400/shop/success',
                 cancel_url='https://pandostores.com/shop/cart',
             )
     return checkout_session
