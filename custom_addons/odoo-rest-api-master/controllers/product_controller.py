550d9ac488596e44c1bc4c00a8c149d965c7b452
Aman Sharma <aman@aman.sharma>
2022-04-12 16:41:05 +0530

    Changes for category product using filter

--- custom_addons/odoo-rest-api-master/controllers/product_controller.py
+++ custom_addons/odoo-rest-api-master/controllers/product_controller.py
@@ -313,10 +313,11 @@ class OdooAPI(http.Controller):
         }
         return return_Response(res)
 
-    @http.route('/api/v1/c/categ/product.template.view', type='http', auth='public', methods=['GET'], csrf=False, cors='*')
+    @http.route('/api/v1/c/categ/product.template.view', type='http', auth='public', methods=['GET'], csrf=False,
+                cors='*')
     def product_template_view_by_categ(self, **params):
         try:
-            domain = [('is_published', '=', True),('type','=','product')]
+            domain = [('is_published', '=', True), ('type', '=', 'product')]
             model = 'product.product'
         except KeyError as e:
             msg = "The model `%s` does not exist." % model
@@ -347,7 +348,13 @@ class OdooAPI(http.Controller):
 
         if "country_id" in params and params.get('country_id'):
             domain.append(('country_id', '=', int(params.get('country_id'))))
-
+        try:
+            jdata = json.loads(request.httprequest.stream.read())
+        except:
+            jdata = {}
+        if jdata and 'attr' in jdata:
+            lst = jdata.get('attr')
+            domain.append(('product_template_attribute_value_ids.product_attribute_value_id', 'in', jdata.get('attr')))
         record_count = request.env[model].sudo().search_count(domain)
         records = request.env[model].sudo().search(domain, order=search, limit=limit, offset=offset)
         if "orderBy" in params and params['orderBy'] == 'featured':
@@ -437,7 +444,8 @@ class OdooAPI(http.Controller):
                              "write_name": i.write_uid.name if i.write_uid.name != False else '',
                              "variants": variant,
                              # "stock": i.qty_available,
-                             "stock": i.with_context(warehouse=warehouse.id).virtual_available if i.with_context(warehouse=warehouse.id).virtual_available>0 else 0.0,
+                             "stock": i.with_context(warehouse=warehouse.id).virtual_available if i.with_context(
+                                 warehouse=warehouse.id).virtual_available > 0 else 0.0,
                              "sm_pictures": image,
                              "featured": i.website_ribbon_id.html if i.website_ribbon_id.html != False else '',
                              "seller_ids": sellers,
@@ -496,12 +504,23 @@ class OdooAPI(http.Controller):
         if "country_id" in params and params.get('country_id'):
             country_id = int(params.get('country_id'))
 
+        try:
+            jdata = json.loads(request.httprequest.stream.read())
+        except:
+            jdata = {}
+        attr = False
+        if jdata and 'attr' in jdata:
+            lst = jdata.get('attr')
+            attr = ('product_template_attribute_value_ids.product_attribute_value_id', 'in', jdata.get('attr'))
+
         if "type" in params and params['type'] == 'popularCategory':
             rec = request.env[model].sudo().search([])
             for j in rec:
                 total_count = 0
                 dom = [('public_categ_ids', 'in', [j.id]), ('is_published', '=', True), ('type', '=', 'product')]
                 domain = dom.append(('country_id', '=', country_id)) if country_id else dom
+                if attr:
+                    domain.append(attr)
                 search_product = request.env['product.product'].sudo().search(domain)
                 for res in search_product:
                     _compute_sales_count(self=res)
@@ -523,9 +542,12 @@ class OdooAPI(http.Controller):
                 domain = [('public_categ_ids', 'in', [i.id]), ('is_published', '=', True), ('type', '=', 'product')]
                 if country_id:
                     domain.append(('country_id', '=', country_id))
+                if attr:
+                    domain.append(attr)
                 search_count = request.env['product.product'].sudo().search_count(domain)
                 temp.append({"id": i.id, "name": i.name,
-                             "image": base_url.value + '/web/image/product.public.category/' + str(i.id) + "/image_1920",
+                             "image": base_url.value + '/web/image/product.public.category/' + str(
+                                 i.id) + "/image_1920",
                              'parent_id': i.parent_id.id if i.parent_id.id != False else '',
                              'parent_name': i.parent_id.name if i.parent_id.name != False else '',
                              "product_count": search_count, 'total_product_sale_count': i.total_sold_count})
@@ -565,7 +587,8 @@ class OdooAPI(http.Controller):
                             domain = [('is_published', '=', True), ('country_id', '=', country_id),
                                       ('name', 'ilike', params['search']), ('type', '=', 'product')]
                         else:
-                            domain = [('is_published', '=', True), ('name', 'ilike', params['search']), ('type', '=', 'product')]
+                            domain = [('is_published', '=', True), ('name', 'ilike', params['search']),
+                                      ('type', '=', 'product')]
 
                 except KeyError as e:
                     msg = "The model `%s` does not exist." % model
@@ -581,96 +604,96 @@ class OdooAPI(http.Controller):
                 [('company_id', '=', website.company_id.id)], limit=1)
             temp = []
             for i in record:
-                    image = []
-                    category = []
-                    variant = []
-                    sellers = []
-                    for j in i.product_template_image_ids:
-                        image.append({"id": j.id, "name": j.name,
-                                      "image": base_url.value + '/web/image/product.image/' + str(j.id) + "/image_1920",
-                                      'url': base_url.value + '/web/image/product.product/' + str(
-                                          j.id) + "/image_1920",
-                                      })
-                    for z in i.public_categ_ids:
-                        category.append({"id": z.id, "name": z.name, "slug": z.name.lower().replace(" ", "-"),
-                                         "image": base_url.value + '/web/image/product.public.category/' + str(
-                                             z.id) + "/image_1920", })
-                    product_var = request.env['product.product'].sudo().search([('id', '=', int(i.id))])
-                    for k in product_var:
-                        values = []
-                        attribute_name = ''
-                        id = []
-                        data = []
-                        for c in k.product_template_attribute_value_ids:
-                            id.append(c.attribute_id.id)
-                        for attr_id in list(set(id)):
-                            for b in k.product_template_attribute_value_ids:
-                                if attr_id == b.attribute_id.id:
-                                    attribute_name = b.attribute_id.name
-                                    if attribute_name.lower() == 'color':
-                                        values.append({"color": b.product_attribute_value_id.name,
-                                                       "color_name": b.product_attribute_value_id.html_color})
-                                    else:
-                                        values.append({"id": b.id, "name": b.name, "slug": None,
-                                                       "pivot": {"components_variants_variant_id": k.id,
-                                                                 "component_id": b.id}})
-                            data.append({attribute_name: values})
-                            values = []
-                        res_data = {"id": k.id, "price": k.list_price,
-                                    "pivot": {"product_id": i.id, "component_id": k.id}}
-
-                        if len(data) != 0:
-                            for dic in data:
-                                res = list(dic.items())[0]
-
-                                # if len
-                                if res[0].lower() == 'color':
-                                    res_data.update(
-                                        {"color": res[1][0].get('color'), "color_name": res[1][0].get('color_name')})
+                image = []
+                category = []
+                variant = []
+                sellers = []
+                for j in i.product_template_image_ids:
+                    image.append({"id": j.id, "name": j.name,
+                                  "image": base_url.value + '/web/image/product.image/' + str(j.id) + "/image_1920",
+                                  'url': base_url.value + '/web/image/product.product/' + str(
+                                      j.id) + "/image_1920",
+                                  })
+                for z in i.public_categ_ids:
+                    category.append({"id": z.id, "name": z.name, "slug": z.name.lower().replace(" ", "-"),
+                                     "image": base_url.value + '/web/image/product.public.category/' + str(
+                                         z.id) + "/image_1920", })
+                product_var = request.env['product.product'].sudo().search([('id', '=', int(i.id))])
+                for k in product_var:
+                    values = []
+                    attribute_name = ''
+                    id = []
+                    data = []
+                    for c in k.product_template_attribute_value_ids:
+                        id.append(c.attribute_id.id)
+                    for attr_id in list(set(id)):
+                        for b in k.product_template_attribute_value_ids:
+                            if attr_id == b.attribute_id.id:
+                                attribute_name = b.attribute_id.name
+                                if attribute_name.lower() == 'color':
+                                    values.append({"color": b.product_attribute_value_id.name,
+                                                   "color_name": b.product_attribute_value_id.html_color})
                                 else:
-                                    res_data.update(dic)
+                                    values.append({"id": b.id, "name": b.name, "slug": None,
+                                                   "pivot": {"components_variants_variant_id": k.id,
+                                                             "component_id": b.id}})
+                        data.append({attribute_name: values})
+                        values = []
+                    res_data = {"id": k.id, "price": k.list_price,
+                                "pivot": {"product_id": i.id, "component_id": k.id}}
 
-                            variant.append(res_data)
-                        else:
-                            pass
+                    if len(data) != 0:
+                        for dic in data:
+                            res = list(dic.items())[0]
 
-                    for n in i.seller_ids:
-                        sellers.append({"id": n.id, "vendor": n.name.name, "vendor_id": n.name.id})
+                            # if len
+                            if res[0].lower() == 'color':
+                                res_data.update(
+                                    {"color": res[1][0].get('color'), "color_name": res[1][0].get('color_name')})
+                            else:
+                                res_data.update(dic)
 
-                    temp.append({"id": i.id, "name": i.name,
-                                 'url': base_url.value + '/web/image/product.product/' + str(i.id) + "/image_1920",
-                                 'image': base_url.value + '/web/image/product.product/' + str(i.id) + "/image_1920",
-                                 'type': i.type, 'sale_price': i.list_price, "price": i.standard_price,
-                                 'description': i.description if i.description != False else '',
-                                 'short_desc': i.description_sale if i.description_sale != False else '',
-                                 'categ_id': i.categ_id.id if i.categ_id.id != False else '',
-                                 'categ_name': i.categ_id.name if i.categ_id.name != False else '',
-                                 "category": category,
-                                 "create_uid": i.create_uid.id if i.create_uid.id != False else '',
-                                 "create_name": i.create_uid.name if i.create_uid.name != False else '',
-                                 "write_uid": i.write_uid.id if i.write_uid.id != False else '',
-                                 "write_name": i.write_uid.name if i.write_uid.name != False else '',
-                                 "variants": variant,
-                                 # "stock": i.qty_available,
-                                 "stock": i.with_context(warehouse=warehouse.id).virtual_available if i.with_context(
-                                     warehouse=warehouse.id).virtual_available > 0 else 0.0,
-                                 "sm_pictures": image,
-                                 "featured": i.website_ribbon_id.html if i.website_ribbon_id.html != False else '',
-                                 "seller_ids": sellers,
-                                 "slug": i.id,
-                                 "top": True if i.website_ribbon_id.html == 'Trending' else None,
-                                 "new": True if i.website_ribbon_id.html == 'New' else None,
-                                 "author": "Pando-Stores",
-                                 "sold": i.sales_count,
-                                 "review": 2,
-                                 "rating": 3,
-                                 "additional_info": i.additional_info if i.additional_info else '',
-                                 "shipping_return": i.shipping_return if i.shipping_return else '',
-                                 "pictures": [{'url': base_url.value + '/web/image/product.product/' + str(
-                                     i.id) + "/image_1920",
-                                               "image": base_url.value + '/web/image/product.product/' + str(
-                                                   i.id) + "/image_1920"}]
-                                 })
+                        variant.append(res_data)
+                    else:
+                        pass
+
+                for n in i.seller_ids:
+                    sellers.append({"id": n.id, "vendor": n.name.name, "vendor_id": n.name.id})
+
+                temp.append({"id": i.id, "name": i.name,
+                             'url': base_url.value + '/web/image/product.product/' + str(i.id) + "/image_1920",
+                             'image': base_url.value + '/web/image/product.product/' + str(i.id) + "/image_1920",
+                             'type': i.type, 'sale_price': i.list_price, "price": i.standard_price,
+                             'description': i.description if i.description != False else '',
+                             'short_desc': i.description_sale if i.description_sale != False else '',
+                             'categ_id': i.categ_id.id if i.categ_id.id != False else '',
+                             'categ_name': i.categ_id.name if i.categ_id.name != False else '',
+                             "category": category,
+                             "create_uid": i.create_uid.id if i.create_uid.id != False else '',
+                             "create_name": i.create_uid.name if i.create_uid.name != False else '',
+                             "write_uid": i.write_uid.id if i.write_uid.id != False else '',
+                             "write_name": i.write_uid.name if i.write_uid.name != False else '',
+                             "variants": variant,
+                             # "stock": i.qty_available,
+                             "stock": i.with_context(warehouse=warehouse.id).virtual_available if i.with_context(
+                                 warehouse=warehouse.id).virtual_available > 0 else 0.0,
+                             "sm_pictures": image,
+                             "featured": i.website_ribbon_id.html if i.website_ribbon_id.html != False else '',
+                             "seller_ids": sellers,
+                             "slug": i.id,
+                             "top": True if i.website_ribbon_id.html == 'Trending' else None,
+                             "new": True if i.website_ribbon_id.html == 'New' else None,
+                             "author": "Pando-Stores",
+                             "sold": i.sales_count,
+                             "review": 2,
+                             "rating": 3,
+                             "additional_info": i.additional_info if i.additional_info else '',
+                             "shipping_return": i.shipping_return if i.shipping_return else '',
+                             "pictures": [{'url': base_url.value + '/web/image/product.product/' + str(
+                                 i.id) + "/image_1920",
+                                           "image": base_url.value + '/web/image/product.product/' + str(
+                                               i.id) + "/image_1920"}]
+                             })
 
         except (SyntaxError, QueryFormatError) as e:
             return error_response(e, e.msg)
@@ -685,9 +708,9 @@ class OdooAPI(http.Controller):
         try:
             model = 'product.attribute'
             record = request.env[model].sudo().search([])
-            attr=[]
+            attr = []
             for rec in record:
-                valueRecord = request.env['product.attribute.value'].sudo().search([('attribute_id','=',rec.id)])
+                valueRecord = request.env['product.attribute.value'].sudo().search([('attribute_id', '=', rec.id)])
                 temp = []
                 for i in valueRecord:
                     vals = {
@@ -698,7 +721,7 @@ class OdooAPI(http.Controller):
                     temp.append(vals)
                 vals = {
                     'attribute_id': rec.id,
-                    'attribute_name':rec.name,
+                    'attribute_name': rec.name,
                     'type': rec.display_type,
                     'value': temp
                 }
