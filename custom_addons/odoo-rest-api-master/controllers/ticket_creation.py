38133ade6c08170ace9693093839f688604cbdd3
Aman Sharma <aman@aman.sharma>
2022-04-27 16:42:22 +0530

    changes in ticket for single ticket

--- custom_addons/odoo-rest-api-master/controllers/ticket_creation.py
+++ custom_addons/odoo-rest-api-master/controllers/ticket_creation.py
@@ -107,6 +107,39 @@ class ControllerREST(http.Controller):
         except (SyntaxError, QueryFormatError) as e:
             return error_response(e, e.msg)
 
+    @http.route('/api/v1/c/getticket/<id>', methods=['GET'], type='http', auth="public", cors='*')
+    @validate_token
+    def get_single_ticket(self, id=None):
+        try:
+            temp = []
+            task = http.request.env['project.task'].sudo().search([('id', '=', int(id))])
+            for i in task:
+                tags = []
+                for z in i.tag_ids:
+                    tags.append({"id": z.id, "name": z.name})
+                vals = {"id": i.id,
+                        "name": i.name if i.name != False else '',
+                        "user_id": i.user_id.id if i.user_id.id != False else '',
+                        "user_name": i.user_id.name if i.user_id.name != False else '',
+                        "stage_id": i.stage_id.id if i.stage_id.id != False else '',
+                        "stage_name": i.stage_id.name if i.stage_id.name != False else '',
+                        "tag_ids": tags,
+                        "partner_id": i.partner_id.id if i.partner_id.id != False else '',
+                        "partner_name": i.partner_id.name if i.partner_id.name != False else '',
+                        "create_date": str(i.create_date),
+                        "deadline_date": str(i.date_deadline),
+                        "ticket_number": i.ticket_number
+                        }
+                temp.append(vals)
+            res = {
+                'data': temp,
+                'message': "Get Successfully ",
+                'status': 200
+            }
+            return return_Response(res)
+        except (SyntaxError, QueryFormatError) as e:
+            return error_response(e, e.msg)
+
     @http.route('/api/v1/c/getAssignedTo', methods=['GET'], type='http', auth="public", cors='*')
     def getAssignedTo(self):
         try:
