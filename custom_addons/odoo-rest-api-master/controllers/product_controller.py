5be4343c68d7ba945e30542afa38eb3923b0bad1
akkygit <aakash.vishwakarma@oodles.io>
2022-05-09 16:52:43 +0530

    changes in product search api for adding filters

--- custom_addons/odoo-rest-api-master/controllers/product_controller.py
+++ custom_addons/odoo-rest-api-master/controllers/product_controller.py
@@ -687,108 +687,25 @@ class OdooAPI(http.Controller):
                 error = {"message": "Something Went Wrong", "status": 400}
                 return return_Response_error(error)
             model = 'product.product'
-            record = request.env[model].sudo().search(domain)
+            search = ''
+            if "filter" in params:
+                if "rating" == params['filter']:
+                    search = 'rating_count DESC'
+                if "new" == params['filter']:
+                    search = 'create_date DESC'
+                if "top" == params['filter']:
+                    search = 'sale_count_pando DESC'
+                if "lth" == params['filter']:
+                    search = 'list_price ASC'
+                if "htl" == params['filter']:
+                    search = 'list_price DESC'
+
+            record = request.env[model].sudo().search(domain, order=search)
             base_url = request.env['ir.config_parameter'].sudo().search([('key', '=', 'web.base.url')], limit=1)
             warehouse = request.env['stock.warehouse'].sudo().search(
                 [('company_id', '=', website.company_id.id)], limit=1)
-            # temp = []
             temp = get_product_details(warehouse, record)
-            # for i in record:
-            #     image = []
-            #     category = []
-            #     variant = []
-            #     sellers = []
-            #     _compute_sales_count(self=i)
-            #     i.sale_count_pando = i.sales_count
-            #     for j in i.product_template_image_ids:
-            #         image.append({"id": j.id, "name": j.name,
-            #                       "image": base_url.value + '/web/image/product.image/' + str(j.id) + "/image_1920",
-            #                       'url': base_url.value + '/web/image/product.product/' + str(
-            #                           j.id) + "/image_1920",
-            #                       })
-            #     for z in i.public_categ_ids:
-            #         category.append({"id": z.id, "name": z.name, "slug": z.name.lower().replace(" ", "-"),
-            #                          "image": base_url.value + '/web/image/product.public.category/' + str(
-            #                              z.id) + "/image_1920", })
-            #     product_var = request.env['product.product'].sudo().search([('id', '=', int(i.id))])
-            #     for k in product_var:
-            #         values = []
-            #         attribute_name = ''
-            #         id = []
-            #         data = []
-            #         for c in k.product_template_attribute_value_ids:
-            #             id.append(c.attribute_id.id)
-            #         for attr_id in list(set(id)):
-            #             for b in k.product_template_attribute_value_ids:
-            #                 if attr_id == b.attribute_id.id:
-            #                     attribute_name = b.attribute_id.name
-            #                     if attribute_name.lower() == 'color':
-            #                         values.append({"color": b.product_attribute_value_id.name,
-            #                                        "color_name": b.product_attribute_value_id.html_color})
-            #                     else:
-            #                         values.append({"id": b.id, "name": b.name, "slug": None,
-            #                                        "pivot": {"components_variants_variant_id": k.id,
-            #                                                  "component_id": b.id}})
-            #             data.append({attribute_name: values})
-            #             values = []
-            #         res_data = {"id": k.id, "price": k.list_price,
-            #                     "pivot": {"product_id": i.id, "component_id": k.id}}
-            #
-            #         if len(data) != 0:
-            #             for dic in data:
-            #                 res = list(dic.items())[0]
-            #
-            #                 # if len
-            #                 if res[0].lower() == 'color':
-            #                     res_data.update(
-            #                         {"color": res[1][0].get('color'), "color_name": res[1][0].get('color_name')})
-            #                 else:
-            #                     res_data.update(dic)
-            #
-            #             variant.append(res_data)
-            #         else:
-            #             pass
-            #
-            #     for n in i.seller_ids:
-            #         sellers.append({"id": n.id, "vendor": n.name.name, "vendor_id": n.name.id})
-            #
-            #     temp.append({"id": i.id, "name": i.name,
-            #                  'url': base_url.value + '/web/image/product.product/' + str(i.id) + "/image_1920",
-            #                  'image': base_url.value + '/web/image/product.product/' + str(i.id) + "/image_1920",
-            #                  'type': i.type, 'sale_price': i.list_price, "price": i.standard_price,
-            #                  'description': i.description if i.description != False else '',
-            #                  'short_desc': i.description_sale if i.description_sale != False else '',
-            #                  'categ_id': i.categ_id.id if i.categ_id.id != False else '',
-            #                  'categ_name': i.categ_id.name if i.categ_id.name != False else '',
-            #                  "category": category,
-            #                  "create_uid": i.create_uid.id if i.create_uid.id != False else '',
-            #                  "create_name": i.create_uid.name if i.create_uid.name != False else '',
-            #                  "write_uid": i.write_uid.id if i.write_uid.id != False else '',
-            #                  "write_name": i.write_uid.name if i.write_uid.name != False else '',
-            #                  "variants": variant,
-            #                  # "stock": i.qty_available,
-            #                  "stock": i.with_context(warehouse=warehouse.id).virtual_available if i.with_context(
-            #                      warehouse=warehouse.id).virtual_available > 0 else 0.0,
-            #                  "sm_pictures": image,
-            #                  "featured": i.website_ribbon_id.html if i.website_ribbon_id.html != False else '',
-            #                  "seller_ids": sellers,
-            #                  "slug": i.id,
-            #                  "top": True if i.website_ribbon_id.html == 'Trending' else None,
-            #                  "new": True if i.website_ribbon_id.html == 'New' else None,
-            #                  "author": "Pando-Stores",
-            #                  "sold": i.sales_count,
-            #                  "review": 2,
-            #                  "rating": 3,
-            #                  "marketplace_seller_id": i.marketplace_seller_id.id,
-            #                  "marketplace_seller_name": i.marketplace_seller_id.name,
-            #                  "additional_info": i.additional_info if i.additional_info else '',
-            #                  "shipping_return": i.shipping_return if i.shipping_return else '',
-            #                  "pictures": [{'url': base_url.value + '/web/image/product.product/' + str(
-            #                      i.id) + "/image_1920",
-            #                                "image": base_url.value + '/web/image/product.product/' + str(
-            #                                    i.id) + "/image_1920"}]
-            #                  })
-
+            
         except (SyntaxError, QueryFormatError) as e:
             return error_response(e, e.msg)
         res = {
