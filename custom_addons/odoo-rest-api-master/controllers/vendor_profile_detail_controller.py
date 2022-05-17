e3b90c92f0f8e3dba56a5d145e09f7d8c04886b5
akkygit <aakash.vishwakarma@oodles.io>
2022-05-17 12:31:04 +0530

    changes in shipping status, added a new state draft

--- custom_addons/odoo-rest-api-master/controllers/vendor_profile_detail_controller.py
+++ custom_addons/odoo-rest-api-master/controllers/vendor_profile_detail_controller.py
@@ -1595,8 +1595,9 @@ class OdooAPI(http.Controller):
                 line = request.env['sale.order.line'].sudo().search([('id', '=', int(line_id))])
                 if "status" in params and params.get('status') and line:
                     line.sudo().write({'shipping_Details': params['status']})
-                error = {"message": "Status is not present in the request", "status": 400}
-                return return_Response_error(error)
+                else:
+                    error = {"message": "Status is not present in the request", "status": 400}
+                    return return_Response_error(error)
         except (SyntaxError, QueryFormatError) as e:
             return error_response(e, e.msg)
         res = {"message": "Status Updated Successfully", "status": 200}
