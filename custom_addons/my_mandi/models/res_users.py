3d4ca7cfe42ff7e6069154d359515ec0c46092eb
akkygit <aakash.vishwakarma@oodles.io>
2022-06-24 19:19:14 +0530

    added user type field in res_users table

--- custom_addons/my_mandi/models/res_users.py
+++ custom_addons/my_mandi/models/res_users.py
@@ -6,6 +6,7 @@ class Users(models.Model):
 
     area_id = fields.Many2one('user.area', string='User Area')
     user_type = fields.Selection([('sm', 'Sale Manager'), ('se', 'Sale Executive'), ('pm', 'Procurement Manager'), ('pe', 'Procurement Executive'), ('wm', 'Warehouse Manager'), ('oe', 'Operational Executive')], string='User Role')
+
 class areaTable(models.Model):
     _name = "user.area"
     _description = "user area store here"
