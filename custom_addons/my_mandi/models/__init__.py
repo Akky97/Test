79a503531e1bdbb871317b88f1c97561674123b4
akkygit <aakash.vishwakarma@oodles.io>
2022-06-26 12:08:12 +0530

    first commit

--- custom_addons/my_mandi/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import sale_order
-from . import procurement
-from . import access_token
-from . import res_users
-
--- custom_addons/my_mandi/models/sale_order.py
+++
@@ -1,8 +0,0 @@
-
-from odoo import api, fields, models, _
-
-
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
-
-    in_process = fields.Boolean(string='In Process', default=False)
